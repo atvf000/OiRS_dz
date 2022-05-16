e2df88013d7d4b749404bdca141cdf672df0e162
atvf000 <47750049+atvf000@users.noreply.github.com>
2022-05-16 21:54:26 +0300

    finish task 01 HW 2

--- task2_01.py
+++ task2_01.py
@@ -1,3 +1,4 @@
+import random
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,6 +15,28 @@ types = ['Bream',
          'Perch',
          'Pike',
          'Smelt']
+
+func_act_names = {
+    0: "Sigmoid",
+    1: "Tanh",
+    2: "ReLU",
+    3: "ELU"
+}
+
+optim_names = {
+    0: "SGD",
+    1: "SGD with momentum",
+    2: "RMSprop",
+    3: "Adam"
+}
+
+
+def kill_random():
+    seed = 1
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.use_deterministic_algorithms(True)
 
 
 def species_to_int(data):
@@ -41,7 +64,7 @@ def prepare(data):
     return X, y, X_test, y_test
 
 
-def create_model(layers=1, func=2, optim_type=2):
+def create_model(layers=1, func=2, optim_type=2, has_batch=False, dropout_prob=None):
     func_activ = {
         0: nn.Sigmoid(),
         1: nn.Tanh(),
@@ -50,9 +73,15 @@ def create_model(layers=1, func=2, optim_type=2):
     }
     model = nn.Sequential()
 
+    if has_batch:
+        model.add_module('b1', nn.BatchNorm1d(10, True))
+
     for i in range(1, layers):
         model.add_module(f'l{i}', nn.Linear(in_features=10, out_features=10))
         model.add_module(f'a{i}', func_activ.get(func))
+
+    if dropout_prob is not None:
+        model.add_module('d1', nn.Dropout(p=dropout_prob))
 
     model.add_module(f'l{layers}', nn.Linear(in_features=10, out_features=1))
 
@@ -60,7 +89,7 @@ def create_model(layers=1, func=2, optim_type=2):
         0: torch.optim.SGD(model.parameters(), lr=0.01, momentum=0),
         1: torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5),
         2: torch.optim.RMSprop(model.parameters(), lr=0.01, alpha=0.5, momentum=0),
-        3: torch.optim.Adam(model.parameters(), lr=0.01)
+        3: torch.optim.Adam(model.parameters())
     }
 
     return model, optims.get(optim_type)
@@ -68,7 +97,6 @@ def create_model(layers=1, func=2, optim_type=2):
 
 def training(X, y, model, optim):
     loss_f = F.mse_loss
-    loss_history = []
 
     for epoch in range(1000):
         optim.zero_grad()
@@ -81,12 +109,18 @@ def training(X, y, model, optim):
 
         optim.step()
 
-        loss_history.append(math.sqrt(loss.item()))
+    return math.sqrt(loss_f(model.forward(X), y).item())
 
-    plt.plot(loss_history)
+
+def predicting(X, y, model):
+    loss_f = F.mse_loss
+    return math.sqrt(loss_f(model.forward(X), y).item())
+
+def print_graph(loss, loss_train):
+    plt.plot(loss, 'ob', label="train")
+    plt.plot(loss_train, 'or', label="test")
+    plt.legend(loc="upper left")
     plt.show()
-    print(loss_history[-1])
-
 
 def start():
     print("Read data from csv")
@@ -98,20 +132,97 @@ def start():
     X, y, X_test, y_test = prepare(data)
 
     loss = []
-    b_amount_layers = 0
+    loss_train = []
 
+    # ----------------------------------------------------------------------
     print("Task 1.3")
+    loss_train.clear()
     loss.clear()
+
     for i in range(1, 5):
         print(f'amount of layers: {i}')
         model, optim = create_model(layers=i)
         training(X, y, model, optim)
-        # TODO add loss append
 
+        loss.append(
+            training(X, y, model, optim)
+        )
+
+        loss_train.append(
+            predicting(X_test, y_test, model)
+        )
+
+        print(loss_train[-1])
+
+    print(loss_train)
+    print_graph(loss, loss_train)
+
+    # ----------------------------------------------------------------------
+    print('\n', '-'*20)
     print("Task 1.4")
+    loss_train.clear()
     loss.clear()
-    for i in range(4):
 
+    for i in range(4):
+        print(f'type of activation function: {func_act_names.get(i)}')
+        model, optim = create_model(layers=4, func=i)
+
+        loss.append(
+            training(X, y, model, optim)
+        )
+
+        loss_train.append(
+            predicting(X_test, y_test, model)
+        )
+        print(loss_train[-1])
+
+    print(loss_train)
+    print_graph(loss, loss_train)
+
+    # ----------------------------------------------------------------------
+    print('\n', '-'*20)
+    print("Task 1.5")
+    loss_train.clear()
+    loss.clear()
+
+    for i in range(4):
+        print(f'type of optimizer: {optim_names.get(i)}')
+        model, optim = create_model(layers=4, func=2, optim_type=i)
+
+        loss.append(
+            training(X, y, model, optim)
+        )
+
+        loss_train.append(
+            predicting(X_test, y_test, model)
+        )
+        print(loss_train[-1])
+
+    print(loss_train)
+    print_graph(loss, loss_train)
+
+    # ----------------------------------------------------------------------
+    print('\n', '-'*20)
+    print("Task 1.6")
+    loss_train.clear()
+    loss.clear()
+
+    for i in [None, 0.2, 0.5]:
+        for j in [False, True]:
+            print(f'has batchNorm: {j}, probability of dropout: {i}')
+            model, optim = create_model(layers=4, func=2, optim_type=2, has_batch=j, dropout_prob=i)
+
+            loss.append(
+                training(X, y, model, optim)
+            )
+
+            loss_train.append(
+                predicting(X_test, y_test, model)
+            )
+            print(loss_train[-1])
+
+    print(loss_train)
+    print_graph(loss, loss_train)
 
 
 if __name__ == '__main__':
